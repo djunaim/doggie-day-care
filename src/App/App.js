433ae57164bce820e38967d4b2cc08dedce5c35c
Monica Djunaidi <modjun12@gmail.com>
2019-12-24 20:12:47 -0600

    have all dogs printed on page

--- src/App/App.js
+++ src/App/App.js
@@ -2,12 +2,26 @@ import React from 'react';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-        <button className="btn btn-danger">Poo</button>
-    </div>
-  );
+import DogPen from '../components/DogPen/DogPen';
+import dogsData from '../helpers/data/dogsData';
+
+class App extends React.Component {
+  state = {
+    dogs: [],
+  }
+
+  componentDidMount() {
+    const dogs = dogsData.getDogs();
+    this.setState({ dogs });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <DogPen dogs={this.state.dogs} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/DogPen/DogPen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import dogShape from '../../helpers/propz/dogShape';
+import Dog from '../Dog/Dog';
+
+class DogPen extends React.Component {
+  static propTypes = {
+    dogs: PropTypes.arrayOf(dogShape.dogShape),
+  }
+
+  render() {
+    const { dogs } = this.props;
+    const dogCards = dogs.map((dog) => <Dog dog={dog} />);
+    return (
+      <div className="container">
+        <div className="row">
+          {dogCards}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default DogPen;
