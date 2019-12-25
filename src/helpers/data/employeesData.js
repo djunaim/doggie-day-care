56f1920da62689b19c518a2c4eb158214f80e676
Monica Djunaidi <modjun12@gmail.com>
2019-12-24 20:21:58 -0600

    created employees data

---
+++ src/helpers/data/employeesData.js
@@ -0,0 +1,30 @@
+const employees = [
+  {
+    id: 'employee1',
+    firstName: 'Alexia',
+    lastName: 'Bourne',
+    phoneNumber: '615-111-1112',
+  },
+  {
+    id: 'employee2',
+    firstName: 'Crystal',
+    lastName: 'Bourne',
+    phoneNumber: '113-000-1112',
+  },
+  {
+    id: 'employee3',
+    firstName: 'Alex',
+    lastName: 'Markus',
+    phoneNumber: '203-867-1112',
+  },
+  {
+    id: 'employee4',
+    firstName: 'Nick',
+    lastName: 'Barkus',
+    phoneNumber: '203-867-1112',
+  },
+];
+
+const getEmployees = () => employees;
+
+export default { getEmployees };
